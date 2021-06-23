a303b20ac6447247a525144e5fea9e2404073dcd
aristizabal144 <aristizabalcristian9005@gmail.com>
2021-06-23 16:02:19 -0500

    Upload project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <i class="fas fa-users"></i>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
+    <Button label="Danger" class="p-button-danger" @click="console()"/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <h1 class="title">Hola mundo</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Button from 'primevue/button';
+
 
 export default Vue.extend({
   name: 'Home',
   components: {
     HelloWorld,
+    Button
   },
+  methods:{
+    console(){
+      console.log("oprimi el boton");
+    }
+  }
 });
 </script>
+
+<style lang="scss" scoped>
+.title{
+  color: $color-primary-company;
+}
+</style>
